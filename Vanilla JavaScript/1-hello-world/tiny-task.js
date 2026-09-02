de9fccac9fc9041e843a7a4971fff723b9ad21fc
Ankita <ankietathorve@gmail.com>
2026-09-02 21:28:52 +0530

    Tiny Coding Task

---
+++ Vanilla JavaScript/1-hello-world/tiny-task.js
@@ -0,0 +1,6 @@
+// Create a JS file that prints your name, age, and goal
+console.log(
+  "Hey, My name is Ankita. I am 32 years old. My goal is to become Frontend Engineer.",
+);
+
+console.log("Name: Ankita, Age: 32, Goal: Frontend Engineer");
