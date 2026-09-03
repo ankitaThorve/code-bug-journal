554081b0c3c617c7591db9cc02d3941822e0692d
Ankita <ankietathorve@gmail.com>
2026-09-03 22:40:21 +0530

    task

---
+++ Vanilla JavaScript/2-variables/2-task.js
@@ -0,0 +1,19 @@
+// Working with variables
+
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let admin, name;
+name = "John";
+admin = name;
+console.log(admin);
+
+// Giving the right name
+// Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let ourPlanetName = "Earth";
+let currentUserName = "Sara";
+
