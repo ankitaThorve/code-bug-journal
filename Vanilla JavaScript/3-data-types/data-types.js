5e2dded4ab9e7dc83de64a5cd4d767d91af1370a
Ankita <ankietathorve@gmail.com>
2026-09-04 14:15:47 +0530

    Numbers

--- Vanilla JavaScript/3-data-types/data-types.js
+++ Vanilla JavaScript/3-data-types/data-types.js
@@ -1,3 +1,17 @@
 // no error
 let message = "hello";
 message = "123456";
+
+// Numbers----------------
+let n = 123; // integers
+n = 12.345; // floating point numbers
+
+console.log(1 / 0); //Infinity
+
+console.log("not a number" / 2); // NaN, such division is erroneous
+
+console.log(NaN + 1); // NaN
+console.log(3 * NaN); // NaN
+console.log("not a number" / 2 - 1); // NaN
+
+console.log(NaN ** 0); // 1
