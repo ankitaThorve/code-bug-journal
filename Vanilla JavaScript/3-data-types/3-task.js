d3ae584212517cd2d5020fe624b2fd98c6097194
Ankita <ankietathorve@gmail.com>
2026-09-04 16:00:31 +0530

    Task done

---
+++ Vanilla JavaScript/3-data-types/3-task.js
@@ -0,0 +1,8 @@
+let name = "Ilya";
+
+// the expression is a number 1
+console.log(`hello ${1}`); // hello 1
+// the expression is a string "name"
+console.log(`hello ${"name"}`); // hello name
+// he expression is a variable, embed it
+console.log(`hello ${name}`);
