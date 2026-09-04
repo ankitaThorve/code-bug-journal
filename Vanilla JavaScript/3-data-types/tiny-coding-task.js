0c870072f5523cdec95a6784b8c7babd4da634c0
Ankita <ankietathorve@gmail.com>
2026-09-04 19:00:55 +0530

    Tiny coding task

---
+++ Vanilla JavaScript/3-data-types/tiny-coding-task.js
@@ -0,0 +1,10 @@
+// Create a small profile using strings, numbers, booleans
+const studentDetails = {
+  firstName: "Dan",
+  lastName: "Humphary",
+  age: 21,
+  contactNumber: 555 - 222 - 333 - 0,
+  isFit: true,
+};
+
+console.log(studentDetails);
