897417551119dc7f410a8bf125dab8a3d3f1f660
Ankita <ankietathorve@gmail.com>
2026-09-04 22:31:17 +0530

    string conversion

---
+++ Vanilla JavaScript/4-type-conversion/type-conversion.js
@@ -0,0 +1,7 @@
+// Type conversion -------------
+// String Conversion---------
+let value = true;
+console.log(typeof value); // boolean
+
+value = String(value); // now value is a string "true"
+console.log(typeof value); // string
