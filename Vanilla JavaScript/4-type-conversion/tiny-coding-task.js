227574845130acf95ccb26a891325feba2690033
Ankita <ankietathorve@gmail.com>
2026-09-05 23:43:55 +0530

    tiny coding task

---
+++ Vanilla JavaScript/4-type-conversion/tiny-coding-task.js
@@ -0,0 +1,12 @@
+// Convert "25" into a number and calculate age next year
+
+// define age as string
+let age = "25";
+
+// convert string into number
+let currentAge = Number(age);
+
+// calculate next years age
+let ageNextYear = currentAge + 1;
+
+console.log(`Sara will be ${ageNextYear} next year.`)
