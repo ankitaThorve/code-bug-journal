30e75f559ef2e39db57d8664f8e6ea195a3481d2
Ankita <ankietathorve@gmail.com>
2026-09-07 16:07:25 +0530

    Tearms: 'unary', 'binary', 'operand'

---
+++ Vanilla JavaScript/5-operators/basic-operators-maths.js
@@ -0,0 +1,18 @@
+// Basic Operators, maths ---------
+// addition +, subtraction -, multiplication *, division / ...
+
+// Tearms: 'unary', 'binary', 'operand'
+let multiplication = 5 * 2; // 5 & 2 are operands
+console.log(multiplication);
+
+// unary-
+// An operator is unary, if it has single operand.
+let x = 1;
+x = -x;
+console.log(x); // -1, unary negation was applied
+
+// An operator is binary, if it has two operands.
+let a = 1,
+  b = 3;
+console.log(b - a); // 2, binary minus subtracts values
+
