b5aa812b623a44d4fcf9140c853fe93bfaa93930
Ankita <ankietathorve@gmail.com>
2026-09-07 16:21:05 +0530

    Maths

--- Vanilla JavaScript/5-operators/basic-operators-maths.js
+++ Vanilla JavaScript/5-operators/basic-operators-maths.js
@@ -1,5 +1,5 @@
 // Basic Operators, maths ---------
-// addition +, subtraction -, multiplication *, division / ...
+// addition +, subtraction -, multiplication *, division / , remainder %, exponentiation **
 
 // Tearms: 'unary', 'binary', 'operand'
 let multiplication = 5 * 2; // 5 & 2 are operands
@@ -16,3 +16,16 @@ let a = 1,
   b = 3;
 console.log(b - a); // 2, binary minus subtracts values
 
+// Remainder % -------------
+console.log(5 % 2); // 1
+console.log(8 % 3); // 2
+console.log(8 % 4); // 0
+
+// Exponentiation ** ----------
+console.log(2 ** 2); // 2² = 4
+console.log(2 ** 3); //2³ = 8
+console.log(2 ** 4); //2⁴ = 16
+
+// Exponentiation ** for non-integer numbers----------
+console.log(4 ** (1 / 2)); // 2 (power of 1/2 is the same as a square root)
+console.log(8 ** (1 / 3)); // 2 (power of 1/3 is the same as a cubic root)
