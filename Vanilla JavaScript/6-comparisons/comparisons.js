f58f1f95ee087d946d310abb15e4fcf4c67bf313
Ankita <ankietathorve@gmail.com>
2026-09-09 19:42:10 +0530

    comparison operators

---
+++ Vanilla JavaScript/6-comparisons/comparisons.js
@@ -0,0 +1,7 @@
+// Comparison operators------------------
+/*
+Greater/less than : a > b , a < b 
+Greater/less than or equals : a >= b, a <= b
+Equals : a == b | equality test | a = b means assignment
+Not equals : a != b
+*/
