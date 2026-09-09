2ba6d9bf952a0d1fac1951078bd31466cc5b67f2
Ankita <ankietathorve@gmail.com>
2026-09-09 19:48:36 +0530

    Boolean is the result

--- Vanilla JavaScript/6-comparisons/comparisons.js
+++ Vanilla JavaScript/6-comparisons/comparisons.js
@@ -5,3 +5,15 @@ Greater/less than or equals : a >= b, a <= b
 Equals : a == b | equality test | a = b means assignment
 Not equals : a != b
 */
+
+// Boolean is the result ---------------------
+/*
+true - 'yes', "correct", "the truth" 
+false - "no", "incorrect", "not the truth"
+*/
+console.log(2 > 1); // true (correct)
+console.log(2 == 1); // false (wrong)
+console.log(2 != 1); // true (correct)
+
+let result = 5 > 4;
+console.log(result); // true
