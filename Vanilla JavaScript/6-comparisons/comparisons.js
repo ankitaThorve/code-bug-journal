b4c52f754d411bf54b68c78518dd5d23c1a9add3
Ankita <ankietathorve@gmail.com>
2026-09-11 12:07:15 +0530

    comparison of different types

--- Vanilla JavaScript/6-comparisons/comparisons.js
+++ Vanilla JavaScript/6-comparisons/comparisons.js
@@ -26,3 +26,25 @@ console.log("Glow" > "Glee"); // true
 console.log("Bee" > "Be"); // true
 
 // Note: 'A' is not equal to 'a' | here 'a' is greater (lowercase)
+
+// Comparison of different types --------------------------
+// JS converts value to number
+console.log("2" > 1); // true, string '2' becomes a number 2
+console.log("01" == 1); // true, string '01' becomes a number 1
+
+// for boolean values, true becomes 1 and false becomes 0
+console.log(true == 1);
+console.log(false == 0);
+
+/* A funny consequence
+>Two values are equal
+>One of them is true as boolean and other one is false as boolean
+From JavaScript’s standpoint, this result is quite normal. An equality check converts values using the numeric conversion (hence "0" becomes 0), while the explicit Boolean conversion uses another set of rules.
+*/
+let a = 0;
+console.log(Boolean(a)); // false
+
+let b = "0";
+console.log(Boolean(b)); // true
+
+console.log(a == b); // true
