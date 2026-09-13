ee24918fb3d2de4c7714fa5bec1e6ef1433158e0
Ankita <ankietathorve@gmail.com>
2026-09-13 15:31:43 +0530

    boolean conversion

--- Vanilla JavaScript/7-conditionals/conditionals.js
+++ Vanilla JavaScript/7-conditionals/conditionals.js
@@ -10,3 +10,22 @@ if (year == 2015) {
   console.log("That's correct");
   console.log("You're so smart!");
 }
+
+// Boolean conversion -----------------------------
+// The if (…) statement evaluates the expression in its parentheses and converts the result to a boolean.
+
+if (0) {
+  // falsy
+  // some code here
+}
+
+if (1) {
+  // truthy
+  // some code here
+}
+
+// We can also pass a pre-evaluated boolean value to if, like this:
+let cond = year == 2015;
+if (cond) {
+  // ...
+}
