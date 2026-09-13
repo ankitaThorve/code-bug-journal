2ea9cd152e3569ea26f0d6936d48e18124712d7b
Ankita <ankietathorve@gmail.com>
2026-09-13 15:11:10 +0530

    if statement

---
+++ Vanilla JavaScript/7-conditionals/conditionals.js
@@ -0,0 +1,12 @@
+// Conditional branching : if, '?'
+
+// ? - conditional operator
+
+// The "if" statement -----------------------
+// The if(...) statement evaluates a condition in parentheses and, if the result is true, executes a block of code.
+
+// wrap multiple code lines in curly braces
+if (year == 2015) {
+  console.log("That's correct");
+  console.log("You're so smart!");
+}
