6cf7f6255a5919514c6bc3bc2d8dcea8245ef57e
Ankita <ankietathorve@gmail.com>
2026-09-14 16:18:07 +0530

    tiny coding task

---
+++ Vanilla JavaScript/7-conditionals/tiny-coding-task.js
@@ -0,0 +1,11 @@
+// Build a grade checker: A/B/C/Fail
+let marks = 87;
+if (marks >= 90) {
+  console.log("A");
+} else if (marks >= 70) {
+  console.log("B");
+} else if (marks >= 50) {
+  console.log("C");
+} else {
+  console.log("Fail");
+}
