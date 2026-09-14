5eecd5e32961c4ea6c68901e6716d8fcbfef286e
Ankita <ankietathorve@gmail.com>
2026-09-14 16:05:21 +0530

    task done

---
+++ Vanilla JavaScript/7-conditionals/7-task.js
@@ -0,0 +1,71 @@
+// Will alert be shown? - if(a string with zero) ----------------------
+if ("0") {
+  console.log("Hello");
+} // yes
+
+// The name of JavaScript -------------------------------
+// Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+
+// If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+let officialName = prompt("What is the “official” name of JavaScript?");
+
+if (officialName == "ECMAScript") {
+  alert("Right!");
+} else {
+  alert("You don't know? ECMAScript!");
+}
+
+// Show the sign ------------------------------
+// Using if...else, write the code which gets a number via prompt and then shows in aleart
+// 1, if the value is greater than zero,
+// -1, if less than zero,
+// 0, if equals zero.
+// In this task we assume that the input is always a number.
+let number = prompt("Enter a number: ", 0);
+if (number > 0) {
+  alert(1);
+} else if (number < 0) {
+  alert(-1);
+} else if (number == 0) {
+  alert(0);
+}
+
+// Rewrite 'if' into '?' ----------------------------------
+// Rewrite this if using the conditional operator '?':
+/*
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+  */
+
+let result = a + b < 4 ? "Below" : "Over";
+
+// Rewrite 'if..else' into '?'
+// Rewrite if..else using multiple ternary operators '?'.
+
+// For readability, it’s recommended to split the code into multiple lines.
+/*
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+  */
+let message =
+  login == "Employee"
+    ? "Hello"
+    : login == "Director"
+      ? "Greetings"
+      : login == ""
+        ? "No Login"
+        : "";
