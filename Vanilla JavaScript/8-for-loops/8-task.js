03d4613859efacaa833ce28d628ef0b7831c3daf
Ankita <ankietathorve@gmail.com>
2026-09-15 15:27:01 +0530

    task

---
+++ Vanilla JavaScript/8-for-loops/8-task.js
@@ -0,0 +1,16 @@
+// 1. Write  a for loop that logs the numbers from 10 down to 1, and then logs 'Lift off!' after the loop finishes
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+console.log("Lift Off!");
+
+// 2. Write a loop that iterates from 1 to 20 and prints only the even number
+for (let j = 2; j <= 20; j += 2) {
+  console.log(`even number : ${j}`);
+}
+
+// 3. Given a number, use a for loop to print its multiplication table up to 10
+let num = 6;
+for (let o = 1; o <= 10; o++) {
+  console.log(`${num} x ${o} = ${num * o} `);
+}
