b562ea7aee4c2e0fde2e2981f94aa690e09c48b3
Ankita <ankietathorve@gmail.com>
2026-09-15 20:00:24 +0530

    while, do...while loops

---
+++ Vanilla JavaScript/9-while-loops/while-do-while.js
@@ -0,0 +1,23 @@
+// while loop -------------------------------
+let i = 0;
+while (i < 3) {
+  console.log(i);
+  i++;
+}
+
+// do...while loop ---------------------------
+/**
+ * syntax :
+ * do{
+ *  // loop body
+ * } while (condition)
+ */
+
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 3);
+
+// The loop will first execute the body, then check the condition, and, while it's truthy, execute it again and again.
+
