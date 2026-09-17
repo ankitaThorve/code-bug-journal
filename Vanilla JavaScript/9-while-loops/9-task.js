6f5eb54c4f6d614fa9bb609817bc29b0bc4dc05b
Ankita <ankietathorve@gmail.com>
2026-09-17 18:19:59 +0530

    task

--- Vanilla JavaScript/9-while-loops/9-task.js
+++ Vanilla JavaScript/9-while-loops/9-task.js
@@ -22,3 +22,53 @@ while (current > 1) {
   current--;
 }
 console.log(`Factorial of ${n} is :`, result);
+
+//
+for (let i = 0; i < 5; ++i) {
+  console.log(i);
+}
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// Use the for loop to output even numbers from 2 to 10
+for (let w = 2; w <= 10; w++) {
+  if (w % 2 == 0) {
+    console.log(w);
+  }
+}
+
+// Replace 'for' with 'while'
+let b = 0;
+while (b < 3) {
+  console.log(`number ${b}!`);
+  b++;
+}
+
+// Repeat until the input is correct
+/*
+Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+*/
+
+let num;
+
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while (num <= 100 && num);
+
+// Output prime numbers
+// Write a code which outputs prime numbers in the intervals from 2 to n
+let p = 10;
+
+nextPrime: for (let i = 2; i <= p; i++) {
+  // for each i ...
+  for (let j = 2; j < i; j++) {
+    // look for a divisor ...
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+  console.log(i); // a prime
+}
